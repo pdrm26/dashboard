cf93b8e0dd5707bb13b397489b4262cdc5081c88
ped <peeeedii@gmail.com>
2023-05-11 17:39:52 +0330

    feat: add Initial fake data

---
+++ src/InitialData.js
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    title: "Macbook pro",
+    category: "Technology",
+    categoryId: 151,
+    price: 2000,
+  },
+  {
+    id: 2,
+    title: "Shampoo",
+    category: "Health",
+    categoryId: 252,
+    price: 10,
+  },
+  { id: 3, title: "Bicycle", category: "Sport", categoryId: 353, price: 200 },
+];
+
+export const categories = [
+  { id: 151, title: "Technology" },
+  { id: 252, title: "Health" },
+  { id: 353, title: "Sport" },
+];
