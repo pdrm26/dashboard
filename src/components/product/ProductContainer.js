cbd71fed5d2fd55f6b79c7f65506820c9005fab7
ped <peeeedii@gmail.com>
2023-05-11 21:22:02 +0330

    feat: when select product item the background change

--- src/components/product/ProductContainer.js
+++ src/components/product/ProductContainer.js
@@ -6,12 +6,17 @@ import { useState } from "react";
 export default function ProductContainer() {
   const [productsList, setProductsList] = useState(products);
   const [currentEditProduct, setCurrentEditProduct] = useState(productsList[0]);
+  const [lastEditProduct, setlastEditProduct] = useState(productsList[0]);
   const onEditProductHandler = (productId) => {
     const productIndex = productsList.findIndex(
       (product) => product.id === productId
     );
-    const newEditableProduct = productsList[productIndex]
-    newEditableProduct.editMode = true
+    const newEditableProduct = productsList[productIndex];
+    if (lastEditProduct) {
+      lastEditProduct.editMode = false;
+    }
+    newEditableProduct.editMode = true;
+    setlastEditProduct(newEditableProduct);
     setCurrentEditProduct(newEditableProduct);
   };
   return (
--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -1,18 +1,18 @@
 export default function ProductItem({ product, onEdit }) {
   const editItemHandler = (productId) => onEdit(productId);
   return (
-    <tr>
+    <tr className={product.editMode ? "selected-product" : ""}>
       <th scope="row">{product.id}</th>
       <td>{product.title}</td>
       <td>{product.category}</td>
       <td>{product.price}</td>
       <td>
-        <button type="button" className="btn btn-danger me-1">
+        <button type="button" className="btn btn-danger btn-sm me-1">
           Remove
         </button>
         <button
           type="button"
-          className="btn btn-warning"
+          className="btn btn-sm btn-warning"
           onClick={editItemHandler.bind(null, product.id)}
         >
           Edit
