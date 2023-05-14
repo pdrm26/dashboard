eee830f916c3e9e7b04f6e8833f3e10fce73a0f7
ped <peeeedii@gmail.com>
2023-05-14 14:17:18 +0330

    feat: add loading comp

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="w-100 text-center">
+      <div className="spinner-border " role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+}
